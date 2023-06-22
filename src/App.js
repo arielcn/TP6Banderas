1f90918607704e557dba2c79da6e6641bb2a14dd
ARIEL DAN COHEN <46914677@ort>
2023-06-22 09:55:17 -0300

    skip y validar terminados

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [ paises, setPaises ] = useState([]);
-  const [ nombre, setNombre ] = useState('');
-  const [ pais, setPais ] = useState({});
+  const [paises, setPaises] = useState([]);
+  const [nombre, setNombre] = useState('');
+  const [pais, setPais] = useState({});
 
   useEffect(() => {
     axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
@@ -17,19 +17,27 @@ function App() {
       });
   }, []);
 
-  const validarName = () => {
+  const validarName = (e) => {
+    e.preventDefault();
     if (nombre === pais.name) {
       console.log("very good")
+      {skipPais()};
     }
+  }
+
+  const skipPais = () => {
+    const paisRandom = paises[Math.floor(Math.random() * paises.length)];
+    setPais(paisRandom);
   }
 
   return (
     <div className="App">
       <h1>{pais.name}</h1>
-      <img width='40%' src={pais.flag}/>
-      <form onSubmit={validarName()}>
+      <img width='40%' src={pais.flag} />
+      <form onSubmit={validarName}>
         <input onKeyUp={(e) => setNombre(e.target.value)} placeholder='nombre del pais' />
         <button type='submit'>Verificar</button>
+        <button onClick={skipPais}>Skip</button>
       </form>
     </div>
   );
